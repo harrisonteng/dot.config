7e8721fcb1389d235936e21b8c1fa3fe7b83991d
harrisonteng <harrisonteng@hotmail.com>
2016-06-02 11:54:33 -0700

    added whois on mybox

---
+++ Ubuntucd.config/settings.ubuntucd/dot.conky/whoisonmybox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MYPTS=$(who -m |sed -e "s%^.* \(pts/[0-9]*\).*(\(.*\))%\1%g");
+if [ "X$MYPTS" == "X" ];then                                                                                                                                                                                                               
+   usrs=`ps auxwww | grep -E '(sshd|sshd:)' |grep -v grep |awk '{print $1" "$2" "$9" "$11" "$12}'`;
+else                                                                                                                                                                                                                                       
+   usrs=`ps auxwww | grep -E '(sshd|sshd:)' |grep -v -E "($MYPTS|sshd -D)"|grep -v grep |awk '{print $1" "$2" "$9" "$11" "$12}'`;
+fi                                                                                                                                                                                                                                         
+
+if [ "X$usrs" != "X" ];then                                                                                                                                                                                                                
+      echo "$usrs";
+fi                                                                                                                                                                                                                                         
