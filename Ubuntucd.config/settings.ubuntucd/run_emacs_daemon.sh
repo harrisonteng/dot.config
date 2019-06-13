acd7926d341bd018f19acfe450650965b7d1b761
harrisonteng <harrisonteng@hotmail.com>
2019-06-12 23:07:32 -0700

    add notes for ipython and isend-mode, add autogen tags

--- Ubuntucd.config/settings.ubuntucd/run_emacs_daemon.sh
+++ Ubuntucd.config/settings.ubuntucd/run_emacs_daemon.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+echo "Gen Master Robot Tags ...";
+find ~/projects.work/repo/GitLab/FortiRobot -name '*.robot' -print | xargs etags -o ~/TAGS/ROBOTMASTER.TAGS --regex='/^[^ \t]\([^\(Library|Resource|Suite|Documentation|#|\$|\*|\[|&|\t|\.|\s\)]*+[A-Z0-9]*+[A-Z0-9]*\).*$/\2/i';
+
+echo "Gen Master Python/Groovy/Shell Tags ...";
+find ~/projects.work/repo/GitLab/FortiRobot /usr/lib/python2.7/dist-packages /home/harrison/projects.work/repo/github/robotframework -type d \( -name '*branches*' -o -name '*tag*' -o -name '*\.ori' -o -name '*\.svn' -o -name '*tmp' -o -name '*temp' \) -prune -o \( -name '*.py' -o -name '*.groovy' -o -name '*.sh' \) -print |xargs -d '\n' etags -a --declarations -o ~/TAGS/PYROBOTMASTER.TAGS;
+
+
 pkill emacs-snapshot;sleep 4;emacs-snapshot --daemon --eval "(setq default-frame-alist '((font-backend . \"xft\") (set-background-color \"black\") (set-foreground-color \"white\")))"
