51b102863d44799246d8af297f9b7bfb53d6e6f6
harrisonteng <harrisonteng@hotmail.com>
2019-08-07 11:15:08 -0700

    add comments

--- Ubuntucd.config/settings.ubuntucd/run_emacs_daemon.sh
+++ Ubuntucd.config/settings.ubuntucd/run_emacs_daemon.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "Gen Master Robot Tags ...";
+echo "Gen Master Robot Tags (ROBOTMASTER)...";
+rm ~/TAGS/ROBOTMASTER.TAGS;
 find ~/projects.work/repo/GitLab/FortiRobot -name '*.robot' -print | xargs etags -o ~/TAGS/ROBOTMASTER.TAGS --regex='/^[^ \t]\([^\(Library|Resource|Suite|Documentation|#|\$|\*|\[|&|\t|\.|\s\)]*+[A-Z0-9]*+[A-Z0-9]*\).*$/\2/i';
 
-echo "Gen Master Python/Groovy/Shell Tags ...";
+echo "Gen Master Python/Groovy/Shell Tags (PYROBOTMASTER)...";
+rm ~/TAGS/PYROBOTMASTER.TAGS;
 find ~/projects.work/repo/GitLab/FortiRobot /usr/lib/python2.7/dist-packages /home/harrison/projects.work/repo/github/robotframework -type d \( -name '*branches*' -o -name '*tag*' -o -name '*\.ori' -o -name '*\.svn' -o -name '*tmp' -o -name '*temp' \) -prune -o \( -name '*.py' -o -name '*.groovy' -o -name '*.sh' \) -print |xargs -d '\n' etags -a --declarations -o ~/TAGS/PYROBOTMASTER.TAGS;
 
 
